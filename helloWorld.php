b2aef61668ad7be56f1aa4d112370c83327bf12f
Amos Wandera <amoswandera37@gmail.com>
2024-02-17 01:00:25 +0300

    first commit

---
+++ helloWorld.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+echo "Hello World! <br>";
+echo "Amos!<br>";
+echo "Hello World!<br>";
+echo "Hello World!<br>";
+ecHo "Hello World!<br>";
+?>
+
+</body>
+</html>
+
+?>
+</body>
+</html>
